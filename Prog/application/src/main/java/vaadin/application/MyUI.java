e0b59b23039aeecc6a62ccaeb364d601ce02c0c0
Unknown <dumitru.georgiana_geanina@yahoo.com>
2017-10-05 13:56:24 +0200

    etwas

--- Prog/application/src/main/java/vaadin/application/MyUI.java
+++ Prog/application/src/main/java/vaadin/application/MyUI.java
@@ -22,6 +22,7 @@ import com.vaadin.ui.VerticalLayout;
 @Theme("mytheme")
 public class MyUI extends UI {
 
+	//Please dont fuck this up
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
